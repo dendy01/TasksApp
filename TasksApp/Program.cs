907d081cfabbe8b4675ce87a98d63f4f22752b61
dendy01 <biomannumberone@gmail.com>
2021-11-24 23:55:16 +0300

    Task A1.6. Fix problem.

--- TasksApp/Program.cs
+++ TasksApp/Program.cs
@@ -2,6 +2,7 @@
 а) сколько полных часов прошло с начала суток;
 б) сколько полных минут прошло с начала очередного часа;
 в) сколько полных секунд прошло с начала очередной минуты.*/
+
 using System;
 
 namespace TasksApp
@@ -12,12 +13,14 @@ namespace TasksApp
         {
             Console.Write("Please specify time  in seconds = ");
             double time = double.Parse(Console.ReadLine());
-            Console.WriteLine("Time in seconds = " + time);
-            double timeHours = (time / 3600);
+
+            int timeHours = (int) time / 3600;
             Console.WriteLine("Full hours have passed sinse the beginning of the day  = " + timeHours);
-            double timeMinuts = (timeHours * 60);
-            Console.WriteLine("Full minuts have passed sinse the beginning of the hours = " + timeMinuts);
-            double timeSeconds = (timeMinuts * 60);
+             
+            int timeMinutes = (int)(time - timeHours * 3600) / 60;
+            Console.WriteLine("Full minuts have passed sinse the beginning of the hours = " + timeMinutes);
+
+            int timeSeconds = (int)(time - timeHours * 3600 - timeMinutes * 60);
             Console.WriteLine("Full seconds have passed the beginning of the minute = " + timeSeconds);
         }
     }
